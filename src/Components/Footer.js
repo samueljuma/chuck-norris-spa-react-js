11f7ab1ca9dd80203a751fffbac84bc9c7c9b490
samueljuma <samueljuma1024@gmail.com>
2021-07-23 00:01:44 +0300

    clean code

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer(){
     return(
         <footer className='bg-gray-200 text-center h-50 text-xs p-3 absolute bottom-0 w-full'>
-            &copy; Copyright 2021 @samueljuma
+            Ⓒ Copyright 2021 SamuelJuma
         </footer>
     )
 }
